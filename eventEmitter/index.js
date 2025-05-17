789acbb8fbcc9396d5eccfd2d5688b0b469033d4
Bachan Kubi <felixghagra@gmail.com>
2025-05-17 13:24:32 +0600

    Learning node js

--- eventEmitter/index.js
+++ eventEmitter/index.js
@@ -1,4 +1,9 @@
 const {EventEmitter} = require('events');
-
+const { emit } = require('process');
+// 1
 const firstEvent = new EventEmitter();
-console.log(firstEvent.emit('ami nai'));
+firstEvent.on('order-pizza', (size, top)=>{
+    console.log(`order received, its baking, a ${size} in size and ${top} on top`);
+});
+// 2
+firstEvent.emit('order-pizza', 'large', 'tomatto');
